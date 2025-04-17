a18485016587d63efa43455cee65681f1dc01b74
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-04-17 16:59:06 +0700

    .

---
+++ app/Models/Team.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Team extends Model
+{
+    /** @use HasFactory<\Database\Factories\TeamFactory> */
+    use HasFactory, Notifiable, HasUuids;
+    protected $table = 'teams';
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+
+     public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+     public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
